cf3ee7b32bcfdda8d20c4bd07ecb9cdc3dd39466
KnsRoo <knsroo@gmail.com>
2021-05-28 12:06:15 +0300

    build

--- .nuxt/middleware.js
+++ .nuxt/middleware.js
@@ -1,6 +1,3 @@
 const middleware = {}
 
-middleware['api'] = require('..\\client\\middleware\\api.js')
-middleware['api'] = middleware['api'].default || middleware['api']
-
 export default middleware
